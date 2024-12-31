6b6fda1e173e27b69105295fe72a7070eb1cfbc1
Orewagenki <orewagenki3@gmail.com>
2024-12-30 22:48:48 -0700

    creating layout width for webpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
 import "./App.css";
+import { Container } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">App</div>;
+  return (
+    <Container>
+      <Grid container>
+        <Grid size={{ xs: 12, sm: 12, md: 4, lg: 3 }} bgcolor="blue">
+          ABC
+        </Grid>
+        <Grid size="grow" bgcolor="red">
+          XYZ
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default App;
