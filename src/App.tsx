9c33a70fb99a2c9a844d575f71a9c1fa9a5fc5c2
Orewagenki <orewagenki3@gmail.com>
2024-12-31 14:39:49 -0700

    Creasted skeleton pages for all my components and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { useState } from "react";
 import "./App.css";
 import { Container } from "@mui/material";
 import Grid from "@mui/material/Grid2";
+import Profile from "./components/profile/Profile";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Portfolio from "./pages/portfolio/Portfolio";
+import Resume from "./pages/resume/Resume";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Container>
       <Grid container>
         <Grid size={{ xs: 12, sm: 12, md: 4, lg: 3 }} bgcolor="blue">
-          ABC
+          <Profile />
         </Grid>
         <Grid size="grow" bgcolor="red">
-          XYZ
+          <Header /> <Portfolio /> <Resume /> <Footer />
         </Grid>
       </Grid>
     </Container>
