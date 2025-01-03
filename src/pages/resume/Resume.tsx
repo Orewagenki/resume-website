617d2a2d79fc7a4ece483fd25d74ed8885c07d26
Orewagenki <orewagenki3@gmail.com>
2025-01-02 17:55:10 -0700

    styled about me section in resume

--- src/pages/resume/Resume.tsx
+++ src/pages/resume/Resume.tsx
@@ -1,21 +1,30 @@
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import "./Resume.css";
 import Grid from "@mui/material/Grid2";
+import resumeData from "../../utils/resumeData";
 
 const Resume = () => {
   return (
     <>
-      <Container className="about_me">
-        <Grid container></Grid>
+      <Container className="section pb_45">
+        <Grid className="section_title mb_30">
+          <span></span>
+          <h6 className="section_title_text">About me</h6>
+        </Grid>
+        <Grid size={{ xs: 12 }}>
+          <Typography variant="body2" className="aboutme_text">
+            {resumeData.about}
+          </Typography>
+        </Grid>
       </Container>
-      <Container className="education_experience">
-        <Grid container></Grid>
+      <Container>
+        <Grid></Grid>
       </Container>
-      <Container className="Skills">
-        <Grid container></Grid>
+      <Container>
+        <Grid></Grid>
       </Container>
-      <Container className="Contact">
-        <Grid container></Grid>
+      <Container>
+        <Grid></Grid>
       </Container>
     </>
   );
